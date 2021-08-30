0dd2362b40547e5e060e6bbdee244e4a361b0b97
thiagohayacizup <thiago.hayaci@zup.com.br>
2021-08-30 13:54:56 -0300

    Versionamento Inicial

---
+++ src/main/java/br/com/projeto/transacao/ProjetoTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/transacao/ProjetoTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
